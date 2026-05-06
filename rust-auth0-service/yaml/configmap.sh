0009025e87299772efd912f4dded9c490f2a3529
tororomeshi <8noki8@gmail.com>
2026-05-06 16:50:28 +0900

    Remove legacy auth config artifacts

--- rust-auth0-service/yaml/configmap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-kubectl create configmap google-auth-config -n auth0 \
-  --from-literal=GOOGLE_REDIRECT_URI="https://auth.tororomeshi.net/auth/google/callback" \
-  --from-literal=APP_BASE_URL="https://portal.tororomeshi.net" \
-  --from-literal=ALLOWED_REDIRECT_ORIGINS="https://portal.tororomeshi.net" \
-  --from-literal=ALLOWED_CORS_ORIGINS="https://portal.tororomeshi.net,https://auth.tororomeshi.net"
